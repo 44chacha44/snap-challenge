b7573d9cf2dd8b8a147f0cf8ad766fa00ae7d7b5
Nehomah Mora <nehomahm@gmail.com>
2019-01-30 09:15:32 -0700

    attempted to answer snap challenge

--- jan29.php
+++ jan29.php
@@ -6,3 +6,16 @@ $map = new \Ds\Map(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
 unset($map[address]);
 var_dump($map);
 ?>
+
+//
+//var array1 = [1, 4, 9, 16];
+
+//pass a function to map
+const map1 = array1
+
+
+
+newArray = array.map(index => {
+return {id:index.id, name:index.name, email:index.email, phone:index.phone, website:index.website}
+})
+console.log(newArray);
