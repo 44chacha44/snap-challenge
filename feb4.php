d072197c23a1dec5578477a5a5bbd98467398145
Nehomah Mora <nehomahm@gmail.com>
2019-02-04 09:11:49 -0700

    1st challenge attempt

--- feb4.php
+++ feb4.php
@@ -2,8 +2,8 @@
 
 /**converts the string into an array and filters out any words that begin with the letter f**/
 
-var effWords = "fate", "fools", "feel", "far", "fire",
-var wiseWords = ["Fate protects fools, little children and ships named Enterprise.
+
+const wiseWords = ["Fate protects fools, little children and ships named Enterprise.
 					You're going to be an interesting companion, Mr. Data. Some days you get the
 					bear, and some days the bear gets you. Maybe if we felt any human loss as
 					keenly as we feel one of those close to us, human history would be far less
@@ -11,4 +11,7 @@ var wiseWords = ["Fate protects fools, little children and ships named Enterpris
 					'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember
 					how to fire phasers? A lot of things can change in twelve years, Admiral."];
 
-newWiseWords = wiseWords.filter(function (str) { return str.includes(effWords); });
+var effWords = (["fate", "fools", "feel", "far", "fire"]);
+
+const mapped = wiseWords.map(val => effwords.has(val) ? "effWords" : val);
+console.log(mapped);
