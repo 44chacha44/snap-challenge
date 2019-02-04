a93e10a7cd8a2d41534e509120230ff98346dfd7
Nehomah Mora <nehomahm@gmail.com>
2019-02-04 08:57:23 -0700

    1st challenge attempt

---
+++ feb4.php
@@ -0,0 +1,14 @@
+<?php
+
+/**converts the string into an array and filters out any words that begin with the letter f**/
+
+var effWords = "fate", "fools", "feel", "far", "fire",
+var wiseWords = ["Fate protects fools, little children and ships named Enterprise.
+					You're going to be an interesting companion, Mr. Data. Some days you get the
+					bear, and some days the bear gets you. Maybe if we felt any human loss as
+					keenly as we feel one of those close to us, human history would be far less
+					bloody. Why don't we just give everybody a promotion and call it a night -
+					'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember
+					how to fire phasers? A lot of things can change in twelve years, Admiral."];
+
+newWiseWords = wiseWords.filter(function (str) { return str.includes(effWords); });
